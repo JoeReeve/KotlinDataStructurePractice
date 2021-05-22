be3dbfe5ff7d620fe5592d688efde73405bfd3e1
Joe Reeve <jreeve36@gmail.com>
2021-05-22 13:43:19 -1000

    added more examples of iteration

--- src/DataStructures.kt
+++ src/DataStructures.kt
@@ -10,11 +10,32 @@ fun main() {
     println("The length of my list is ${myStringList.size}")
     println("The length of my set is ${myStringSet.size}")
 
+    for (string in myStringArray) {
+        println(string)
+    }
+
+    myStringSet.forEach {
+        println(it)
+    }
+
+    myStringSet.forEach {string ->
+        println(string)
+    }
+    
+    myStringList.forEachIndexed { index, stringItem ->
+        println("$stringItem is at index $index")
+    }
+
+
     //Maps
     val userMap = mapOf("user-001" to "John", Pair("user-002",
     "Marcus"))
     println(userMap.entries)
     println("${userMap["user-001"]}")
+
+    val map = mutableMapOf(1 to "a", 2 to "b", 3 to "c")
+    map.forEach {key, value -> println("$key -> $value")}
+    map.put(4, "d")
 
     //Data Classes
     val personMap = mapOf("Marcus" to Person("Marcus", "Johnson", listOf("Marky", "Johnny")))
